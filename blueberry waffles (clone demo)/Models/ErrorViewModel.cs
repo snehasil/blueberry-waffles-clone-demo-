15a4d7c2ea542e45605d2617fad92ce3afeb3e78
Sneha Sil <snehasil413@gmail.com>
2021-06-13 14:40:38 -0500

    created project

---
+++ blueberry waffles (clone demo)/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace blueberry_waffles__clone_demo_.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
